9b106b1cb1f3dd7a08879bebc39c05fbc607bb94
xinj10 <xinj10@outlook.com>
2023-12-12 20:31:06 +0800

    feat: finish basic

--- main.js
+++ main.js
@@ -4,23 +4,26 @@ const operatorBtns = document.querySelectorAll(".operator");
 const equalBtn = document.querySelector(".equal");
 const clearBtn = document.querySelector(".clear");
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => {
-  if (b === 0) return "ERROR: divide by 0";
-  return Math.floor((a / b) * 100) / 100; // round to 2 decimal
-};
-
-let operandA;
-let operandB;
-let operator;
+let operandA = "";
+let operandB = "";
+let operator = "";
 
 const state = {
   a: false, //operand A
   b: false, // operand B
   o: false, //operator
-  e: false,
+  e: false, // if it's in error state
+};
+
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+  if (b === 0) {
+    state.e = true;
+    return "ERROR: divide by 0";
+  }
+  return Math.floor((a / b) * 100) / 100; // round to 2 decimal
 };
 
 function operate(a, b, op) {
@@ -48,23 +51,118 @@ function display(displayValue) {
   displayElem.textContent = displayValue;
 }
 
+function initCal() {
+  state.a = false;
+  state.b = false;
+  state.o = false;
+  state.e = false;
+  operandA = "";
+  operandB = "";
+  operator = "";
+  display("Hello");
+}
+
+clearBtn.addEventListener("click", initCal);
+
 numberBtns.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    display();
-  })
-);
-operatorBtns.forEach((btn) =>
   btn.addEventListener("click", (event) => {
-    operandA = Number.parseInt(displayValue, 10);
-    operator = event.target.textContent;
-    displayValue = "";
-    displayElem.textContent = operator;
+    if (state.e) {
+      initCal();
+      return;
+    }
+    if (!state.a && !state.b && !state.o) {
+      state.a = true;
+      operandA = operandA + event.target.textContent;
+      display(operandA);
+      return;
+    }
+    if (state.a && !state.b && !state.o) {
+      operandA = operandA + event.target.textContent;
+      display(operandA);
+      return;
+    }
+    if (state.a && !state.b && state.o) {
+      operandB = operandB + event.target.textContent;
+      state.b = true;
+      display(operandB);
+      return;
+    }
+    if (state.a && state.b && state.o) {
+      operandB = operandB + event.target.textContent;
+      display(operandB);
+      return;
+    }
   })
 );
+
+operatorBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    if (state.e) {
+      initCal();
+      return;
+    }
+    if (!state.a && !state.b && !state.o) {
+      display("ERROR: invalid input");
+      state.e = true;
+      return;
+    }
+    if (state.a && !state.b && !state.o) {
+      state.o = true;
+      operator = event.target.textContent;
+      return;
+    }
+    if (state.a && !state.b && state.o) {
+      display("ERROR: invalid input");
+      state.e = true;
+      return;
+    }
+    if (state.a && state.b && state.o) {
+      const result = operate(
+        Number.parseInt(operandA, 10),
+        Number.parseInt(operandB, 10),
+        operator
+      );
+      operandA = typeof result === "string" ? "" : result.toString();
+      state.a = true;
+      opertor = event.target.textContent;
+      state.o = true;
+      operandB = "";
+      state.b = false;
+      display(result);
+    }
+  });
+});
+
 equalBtn.addEventListener("click", (event) => {
-  operandB = Number.parseInt(displayValue, 10);
-  const result = operate(operandA, operandB, operator);
-  operandA = 0;
-  operandB = 0;
-  displayElem.textContent = result;
+  if (state.e) {
+    initCal();
+    return;
+  }
+  if (!state.a && !state.b && !state.o) {
+    display("ERROR: invalid input");
+    state.e = true;
+    return;
+  }
+  if (state.a && !state.b && !state.o) {
+    return;
+  }
+  if (state.a && !state.b && state.o) {
+    display("ERROR: invalid input");
+    state.e = true;
+    return;
+  }
+  if (state.a && state.b && state.o) {
+    const result = operate(
+      Number.parseInt(operandA, 10),
+      Number.parseInt(operandB, 10),
+      operator
+    );
+    operandA = typeof result === "string" ? "" : result.toString();
+    state.a = true;
+    operandB = "";
+    state.b = false;
+    operator = "";
+    state.o = false;
+    display(result);
+  }
 });
